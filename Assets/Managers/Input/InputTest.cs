55ec21125f8e4d967abbe4098d458e2198ebe229
Jordi Planas Martinez <jordi.planas@estudiant.enti.cat>
2025-11-06 19:37:51 +0100

    InputManager with Actions
    Singleton InputManager with character input handlers, control scheme state and vibration methods.

---
+++ Assets/Managers/Input/InputTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(InputManager))]
+public class InputTest : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        InputManager.Instance.OnMovement += Display2D;
+        InputManager.Instance.OnJump += DisplayBool;
+        InputManager.Instance.OnDive += DisplayBool;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Instance.OnMovement += Display2D;
+        InputManager.Instance.OnJump += DisplayBool;
+        InputManager.Instance.OnDive += DisplayBool;
+    }
+    
+    private void DisplayBool(bool obj)
+    {
+        Debug.Log(obj);
+    }
+
+    private void Display2D(Vector2 obj)
+    {
+        Debug.Log(obj);
+    }
+}
