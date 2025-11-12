551eb8f693f4580f6f1238f36e076e1eda819992
Adrián <adrian.fernandez@enti.cat>
2025-11-12 02:40:16 +0100

    Movement reworked and jump working

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.InputSystem.XR;
@@ -8,39 +9,81 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
     private Animator anim;
 
-    [SerializeField] private float velocity;
-    [SerializeField] private float fallVel;
+    private PlayerGroundDetection groundDetection;
 
+    private float playerGravity = 9.81f;
+
+    [SerializeField] private float velocity;
+
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpTimer;
+    [SerializeField] private float actualJumpTimer;
+
+    [SerializeField] private float linearDamping;
+
+    private Vector2 axis;
 
     void Start()
     {
         tr = GetComponent<Transform>();
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+
+        groundDetection = GetComponentInChildren<PlayerGroundDetection>();
     }
 
     private void OnEnable()
     {
-        InputManager.Instance.OnMovement += Movement;
+        InputManager.Instance.OnMovement += SetAxis;
+        InputManager.Instance.OnJump += Jump;
     }
 
     private void OnDisable()
     {
-        InputManager.Instance.OnMovement -= Movement;
+        InputManager.Instance.OnMovement -= SetAxis;
+        InputManager.Instance.OnJump -= Jump;
+
     }
+
+    private void SetAxis(Vector2 input) { axis = input; }
 
     void Update()
     {
-        
+
+
+        if (groundDetection.OnGround())
+        {
+            rb.linearDamping = linearDamping;
+        }
+        else
+        {
+            rb.linearDamping = 0.0f;
+        }
     }
 
-    private void Movement(Vector2 axis)
+    private void FixedUpdate()
+    {
+        if (actualJumpTimer > 0)
+        {
+            actualJumpTimer -= Time.fixedDeltaTime;
+
+            rb.AddForce(Vector3.up * jumpForce * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+
+        Movement();
+
+        //GRAVEDAD
+        //Vector3 gravity = playerGravity * rb.mass * Vector3.up;
+        //rb.AddForce(-gravity, ForceMode.Acceleration);
+    }
+
+    private void Movement()
     {
         if(axis != Vector2.zero)
         {
             float tan;
 
-            tan = (Mathf.Atan(axis.x / axis.y) * Mathf.Rad2Deg);
+            tan = Mathf.Atan(axis.x / axis.y) * Mathf.Rad2Deg;
 
             if (axis.y < 0)
             {
@@ -54,16 +97,35 @@ public class PlayerMovement : MonoBehaviour
 
             tr.rotation = Quaternion.Euler(0, tan, 0);
 
+            Vector3 targetXZ = tr.forward.normalized * velocity * axis.magnitude;
 
-            rb.linearVelocity = (tr.forward.normalized * velocity) + (Vector3.up * rb.linearVelocity.y);
+            Vector3 target = new Vector3(targetXZ.x, rb.linearVelocity.y, targetXZ.z);
+
+            Vector3 deltaV = target - rb.linearVelocity;
+
+            rb.AddForce(deltaV, ForceMode.VelocityChange);
+
 
             anim.SetBool("Walk", true);
+            anim.SetFloat("WalkVel", axis.magnitude);
         }
         else
         {
-            rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, 0);
-
             anim.SetBool("Walk", false);
+        }
+    }
+
+    private void Jump(bool jump)
+    {
+        if (groundDetection.OnGround() && jump)
+        {
+            actualJumpTimer = jumpTimer;
+            anim.SetTrigger("Jump");
+
+        }
+        else if(!jump)
+        {
+            actualJumpTimer = 0.0f;
         }
     }
 }
